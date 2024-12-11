44eb0ec8624a75750f064ed253ddc6ea87b97a69
Relix <54997892+Relix25@users.noreply.github.com>
2024-12-11 14:03:03 +0000

    Ajout du code dans Exmployee.java

---
+++ src/main/java/Employee.java
@@ -0,0 +1,22 @@
+/**
+* Représente un employé dans une entreprise.
+*/
+public class Employee {
+    private String nom;
+    private int id;
+    private String position;
+
+    /**
+     * Un truc
+     * @param nom
+     * @param id
+     * @param position
+     */
+    public Employee(String nom, int id, String position){
+
+    }
+    // TODO: Ajoutez les attributs ici (nom, id, position).
+    // TODO: Ajoutez un constructeur pour initialiser les attributs.
+    // TODO: Ajoutez des méthodes pour récupérer les valeurs des attributs (getters).
+    // TODO: Ajoutez une méthode toString() pour représenter un employé en texte.
+    }
